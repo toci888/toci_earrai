5d136d9156f4b4dfcd08d11a2f562149f56400ff
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-08-24 13:52:39 +0200

    Tomek gitignore :P fix :P

--- Toci.Earrai/Toci.Earrai.Bll/WorksheetLogic.cs
+++ Toci.Earrai/Toci.Earrai.Bll/WorksheetLogic.cs
@@ -8,6 +8,7 @@ using Toci.Common.Database.Interfaces;
 using Toci.Earrai.Bll.Models;
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
+using Toci.Earrai.Bll.Interfaces;
 
 namespace Toci.Earrai.Bll {
     public class WorksheetLogic : Logic<Worksheet>, IWorksheetLogic {
