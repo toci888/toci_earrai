3395b40f0991b7ee3cb28284b9b20a47f42710e6
Tomek <tomek1989gw@wppl>
2021-08-25 14:27:25 +0200

    Test ExcelTableTrimmer

---
+++ Toci.Earrai/Toci.Microsoft.Graph.Excel/TableTrimer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Microsoft.Graph.Excel {
+    class TableTrimer {
+
+
+        public virtual void trimExcelArray(object excelArray)
+        {
+
+            
+
+
+        }
+
+
+    }
+}
