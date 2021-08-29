181309569e3fc94d9710c989efdb00f1f51700be
eb110 <wfigura@op.pl>
2021-08-29 19:37:03 +0100

    Areas and CodesDimension plus AreaQuantity endpoints (general get only)

---
+++ Toci.Earrai/Toci.Earrai.Bll.Interfaces/IAreasLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Common.Bll.Interfaces;
+using Toci.Earrai.Database.Persistence.Models;
+
+namespace Toci.Earrai.Bll.Interfaces
+{
+    public interface IAreasLogic : ILogicBase<Area>
+    {
+        List<Area> GetAllAreasFromDb();
+
+        List<Codesdimension> GetAllCodesDimensionsFromDb();
+    }
+}
---
+++ Toci.Earrai/Toci.Earrai.Bll/AreaquantityLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Earrai.Bll.Interfaces;
+using Toci.Earrai.Database.Persistence.Models;
+
+namespace Toci.Earrai.Bll
+{
+    public class AreaquantityLogic : Logic<Areaquantity>, IAreaquantityLogic
+    {
+        public List<Areaquantity> GetAllAreaQuantitiesFromDb()
+        {
+            Logic<Areaquantity> areaQuantities = new Logic<Areaquantity>();
+
+            return areaQuantities.Select(m => true).ToList();
+        }
+    }
+}
