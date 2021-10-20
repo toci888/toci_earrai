6b90965a561e6906cdb27630a827b90903be3182
Tomek <tomek1989gw@wp.pl>
2021-10-20 19:04:35 +0200

    ???

--- Toci.Earrai/Toci.Earrai.Bll.Models/Calculation/KgPerSqrtMeterDto.cs
+++ Toci.Earrai/Toci.Earrai.Bll.Models/Calculation/KgPerSqrtMeterDto.cs
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 using Toci.Earrai.Database.Persistence.Models;
 
 namespace Toci.Earrai.Bll.Models.Calculation {
-    public class KgPerSqrtMeterDto: CalculationsBaseDto {
+    public class KgPerSqrtMeterDto : CalculationsBaseDto {
 
         protected const int Density = 8500;
 
         protected const int DivideBy = 1000;
+
+        private double thickness_;
+
+        public double Thickness_ {
+            get { return thickness_; }
+            set {
+                thickness_ = value;
+            }
+        }
+
+        public override void AssignValues(List<Productssize> productsizes) {
+
+            foreach (var dimensionType in productsizes) {
+
+                if (dimensionType.Name == "Thickness") {
+                    thickness_ = float.Parse(dimensionType.Value);
+                }
+
+            }
+            
+        }
 
         public int getValue() {
             return Density / DivideBy;
