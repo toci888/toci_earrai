a176de8ea6e9693303b4e64a342b6792a7be670f
kacper8989 <kacper.wybierala1@gmail.com>
2021-10-21 18:09:05 +0200

    class from discord :)

---
+++ Toci.Earrai/Toci.Earrai.Bll/ProductParams/FLTS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Earrai.Bll.Models.Calculation;
+
+namespace Toci.Earrai.Bll.ProductParams
+{
+    public class FLTS : ProductParamsBase
+    {
+        public override CalculationsBaseDto GetProductParams(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
