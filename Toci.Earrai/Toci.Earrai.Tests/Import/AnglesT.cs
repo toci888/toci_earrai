f3cbf6416512b92655cfbe5f54582fc0e8a10b4b
kacper8989 <kacper.wybierala1@gmail.com>
2021-10-22 17:18:11 +0200

    All imports

---
+++ Toci.Earrai/Toci.Earrai.Tests/Import/AnglesT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Toci.Earrai.Bll;
+using Toci.Earrai.Database.Persistence.Models;
+
+namespace Toci.Earrai.Tests.Import
+{
+    public class AnglesT : ImportBase
+    {
+        protected override void ImportAreas(List<string> row, int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ImportSizes(List<string> row, int productId)
+        {
+            ProductSizeLogic.Insert(new Productsize() { Idproducts = productId, Idsizes = (int)ProductSizesEnum.Thickness, Value = row[5] });
+        }
+
+        protected override void ImportOptions(List<string> row, int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ImportPricing(List<string> row, int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Toci.Earrai/Toci.Earrai.Tests/Import/ChanBms.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Toci.Earrai.Bll;
+using Toci.Earrai.Database.Persistence.Models;
+
+namespace Toci.Earrai.Tests.Import
+{
+    public class ChanBms : ImportBase
+    {
+        protected override void ImportAreas(List<string> row, int productId)
+        {
+            //ProductSizeLogic.Insert(new Productsize() { Idproducts = productId, Idsizes = (int)ProductSizesEnum.Length, Value = row[4] });
+            //ProductSizeLogic.Insert(new Productsize() { Idproducts = productId, Idsizes = (int)ProductSizesEnum.Width, Value = row[5] });
+            //ProductSizeLogic.Insert(new Productsize() { Idproducts = productId, Idsizes = (int)ProductSizesEnum.Thickness, Value = row[6] });
+        }
+
+        protected override void ImportSizes(List<string> row, int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ImportOptions(List<string> row, int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void ImportPricing(List<string> row, int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
