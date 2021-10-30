7dd96f12e53d645fe96af75b5108e8c24201878a
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-10-30 08:37:43 +0200

    pring draft.

---
+++ Toci.Earrai/Toci.Earrai.Bll/Calculations/Pricing/PricesEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Earrai.Bll.Calculations.Pricing
+{
+    public enum PricesEnum
+    {
+        Tonne = 1,
+        Sheet = 2,
+        Meter = 3,
+        SquareMeter = 4,
+        Length = 5
+    }
+}
