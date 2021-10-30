cb6d5dc1f0783d64a04d7ccb3c3bc393120dcfd4
Tomek <tomek1989gw@wp.pl>
2021-10-30 09:23:29 +0200

    x

---
+++ Toci.Earrai/Toci.Earrai.Bll/Calculations/Pricing/ChanBmsCalculation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Earrai.Bll.Models;
+
+namespace Toci.Earrai.Bll.Calculations.Pricing {
+    class ChanBmsCalculation : PriceCalculationBase {
+        protected override PricingDto PricePerSheet(ProductDto product, PricingDto dto) {
+            throw new NotImplementedException();
+        }
+    }
+}
