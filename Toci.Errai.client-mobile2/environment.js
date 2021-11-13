7dc7b59c3cd73909b9aa1bd791d30ac476690868
Tomek <tomek1989gw@wp.pl>
2021-11-13 13:05:29 +0100

    dupka

--- Toci.Errai.client-mobile2/environment.js
+++ Toci.Errai.client-mobile2/environment.js
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://20.49.181.92/',
-    prodApiUrl: 'http://20.49.181.92/'
+    apiUrl: 'http://localhost:18158/',
+      prodApiUrl: 'http://20.49.181.92/'
 };
---
+++ Toci.Errai.client-mobile2/shared/tempConfig.js
@@ -0,0 +1,473 @@
+
+export const loginUserData = {
+    "id": 6,
+    "firstname": "tom123",
+    "lastname": "tom123",
+    "email": "tom123@wp.pl",
+    "password": "BF91DF79A0C1DB76D19817BF00D30631981B7D11BFB85A821E6527E62542C801",
+    "emailconfirmed": 0,
+    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjYiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoidG9tMTIzIHRvbTEyMyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlVzZXIiLCJleHAiOjE2Mzc0OTk4NzMsImlzcyI6Imh0dHA6Ly9lYXJyYWkuY29tIiwiYXVkIjoiaHR0cDovL2VhcnJhaS5jb20ifQ.gxa1uzlA3eoFsNLmCkN0Hm_DYLnBDar7EiTSbGLtxqY",
+    "name": "User"
+}
+
+
+export const productsList = [
+    {
+        "id": 1,
+        "idcategories": 1,
+        "idworksheet": 1,
+        "rowindex": null,
+        "productaccountreference": "PL_1_2500_1250",
+        "description": "PL_1_2500_1250 @ 7.85Kg/m2",
+        "idcategoriesNavigation": null,
+        "idworksheetNavigation": null,
+        "areaquantities": [],
+        "productoptionvalues": [],
+        "productsizes": [],
+        "quoteandprices": []
+    },
+    {
+        "id": 2,
+        "idcategories": 1,
+        "idworksheet": 1,
+        "rowindex": null,
+        "productaccountreference": "PL_1_2500_1250",
+        "description": "PL_1_2500_1250 @ 7.85Kg/m2",
+        "idcategoriesNavigation": null,
+        "idworksheetNavigation": null,
+        "areaquantities": [],
+        "productoptionvalues": [],
+        "productsizes": [],
+        "quoteandprices": []
+    },
+    {
+        "id": 3,
+        "idcategories": 1,
+        "idworksheet": 1,
+        "rowindex": null,
+        "productaccountreference": "PL_1.2__",
+        "description": "PL_1.2__ @ 9.42Kg/m2",
+        "idcategoriesNavigation": null,
+        "idworksheetNavigation": null,
+        "areaquantities": [],
+        "productoptionvalues": [],
+        "productsizes": [],
+        "quoteandprices": []
+    },
+    {
+        "id": 4,
+        "idcategories": 1,
+        "idworksheet": 1,
+        "rowindex": null,
+        "productaccountreference": "PL_1.5_2500_1250",
+        "description": "PL_1.5_2500_1250 @ 11.775Kg/m2",
+        "idcategoriesNavigation": null,
+        "idworksheetNavigation": null,
+        "areaquantities": [],
+        "productoptionvalues": [],
+        "productsizes": [],
+        "quoteandprices": []
+    },
+    {
+        "id": 5,
+        "idcategories": 1,
+        "idworksheet": 1,
+        "rowindex": null,
+        "productaccountreference": "PL_2_2500_1250",
+        "description": "PL_2_2500_1250 @ 15.7Kg/m2",
+        "idcategoriesNavigation": null,
+        "idworksheetNavigation": null,
+        "areaquantities": [],
+        "productoptionvalues": [],
+        "productsizes": [],
+        "quoteandprices": []
+    }
+]
+
+export const loginData = [
+    [
+        {
+            "id": 1,
+            "sheetname": "PLT & SHEET",
+            "createdat": null,
+            "updatedat": null,
+            "products": [],
+            "worksheetcontents": [],
+            "worksheetcontentshistories": []
+        }
+    ],
+    [
+        {
+            "id": 1,
+            "code": "RH",
+            "name": "Rack House",
+            "areaquantities": []
+        },
+        {
+            "id": 2,
+            "code": "CO",
+            "name": "Compound",
+            "areaquantities": []
+        },
+        {
+            "id": 3,
+            "code": "BW",
+            "name": "Bottom Workshop",
+            "areaquantities": []
+        },
+        {
+            "id": 4,
+            "code": "TS",
+            "name": "Top Shed",
+            "areaquantities": []
+        },
+        {
+            "id": 5,
+            "code": "BY",
+            "name": "Back Yard",
+            "areaquantities": []
+        },
+        {
+            "id": 6,
+            "code": "FY",
+            "name": "Front Yard",
+            "areaquantities": []
+        },
+        {
+            "id": 7,
+            "code": "BP",
+            "name": "Big Press",
+            "areaquantities": []
+        },
+        {
+            "id": 8,
+            "code": "PO",
+            "name": "Porch",
+            "areaquantities": []
+        },
+        {
+            "id": 9,
+            "code": "BS",
+            "name": "Bolt Store",
+            "areaquantities": []
+        },
+        {
+            "id": 10,
+            "code": "LO",
+            "name": "Loft",
+            "areaquantities": []
+        },
+        {
+            "id": 11,
+            "code": "GY",
+            "name": "Garages",
+            "areaquantities": []
+        },
+        {
+            "id": 12,
+            "code": "BG",
+            "name": "Behind Garages",
+            "areaquantities": []
+        },
+        {
+            "id": 13,
+            "code": "81",
+            "name": "81 Dwelling House",
+            "areaquantities": []
+        },
+        {
+            "id": 14,
+            "code": "QA",
+            "name": "Quarry Left of Main Route",
+            "areaquantities": []
+        },
+        {
+            "id": 15,
+            "code": "QB",
+            "name": "Quarry Front Shed",
+            "areaquantities": []
+        },
+        {
+            "id": 16,
+            "code": "QC",
+            "name": "Quarry Back Shed",
+            "areaquantities": []
+        },
+        {
+            "id": 17,
+            "code": "QD",
+            "name": "Quarry Concrete",
+            "areaquantities": []
+        },
+        {
+            "id": 18,
+            "code": "QE",
+            "name": "Quarry Area E",
+            "areaquantities": []
+        },
+        {
+            "id": 19,
+            "code": "QF",
+            "name": "Quarry Area F",
+            "areaquantities": []
+        },
+        {
+            "id": 20,
+            "code": "QG",
+            "name": "Quarry Area G",
+            "areaquantities": []
+        },
+        {
+            "id": 21,
+            "code": "QH",
+            "name": "Quarry Area H",
+            "areaquantities": []
+        },
+        {
+            "id": 22,
+            "code": "QI",
+            "name": "Quarry Area I",
+            "areaquantities": []
+        },
+        {
+            "id": 23,
+            "code": "QJ",
+            "name": "Quarry Area J",
+            "areaquantities": []
+        },
+        {
+            "id": 24,
+            "code": "QK",
+            "name": "Quarry Area K",
+            "areaquantities": []
+        },
+        {
+            "id": 25,
+            "code": "QL",
+            "name": "Quarry Area L",
+            "areaquantities": []
+        },
+        {
+            "id": 26,
+            "code": "QM",
+            "name": "Quarry Area M",
+            "areaquantities": []
+        },
+        {
+            "id": 27,
+            "code": "empty",
+            "name": "empty",
+            "areaquantities": []
+        }
+    ],
+    [
+        {
+            "id": 1,
+            "name": "McCance",
+            "quoteandprices": []
+        },
+        {
+            "id": 2,
+            "name": "Wwatson",
+            "quoteandprices": []
+        },
+        {
+            "id": 3,
+            "name": "McArdle",
+            "quoteandprices": []
+        },
+        {
+            "id": 4,
+            "name": "Hannon",
+            "quoteandprices": []
+        },
+        {
+            "id": 5,
+            "name": "RFL",
+            "quoteandprices": []
+        }
+    ],
+    [
+        {
+            "id": 1,
+            "name": "PoundsPerTonne",
+            "valuation": "£/T",
+            "quoteandprices": []
+        },
+        {
+            "id": 2,
+            "name": "PoundsPerLength",
+            "valuation": "£/L",
+            "quoteandprices": []
+        },
+        {
+            "id": 3,
+            "name": "PoundsPerMeter",
+            "valuation": "£/m",
+            "quoteandprices": []
+        },
+        {
+            "id": 4,
+            "name": "PoundsPerSheet",
+            "valuation": "£/Sht",
+            "quoteandprices": []
+        }
+    ]
+]
+
+
+export const productData = {
+    "product": {
+        "id": 1,
+        "idcategories": 1,
+        "idworksheet": 1,
+        "rowindex": null,
+        "productaccountreference": "PL_1_2500_1250",
+        "description": "PL_1_2500_1250 @ 7.85Kg/m2",
+        "idcategoriesNavigation": null,
+        "idworksheetNavigation": null,
+        "areaquantities": [],
+        "productoptionvalues": [],
+        "productsizes": [],
+        "quoteandprices": []
+    },
+    "options": [],
+    "sizes": [
+        {
+            "id": 1,
+            "idproducts": 1,
+            "value": "2500",
+            "name": "Length"
+        },
+        {
+            "id": 2,
+            "idproducts": 1,
+            "value": "1250",
+            "name": "Width"
+        },
+        {
+            "id": 3,
+            "idproducts": 1,
+            "value": "1",
+            "name": "Thickness"
+        }
+    ],
+    "prices": [
+        {
+            "idproducts": 1,
+            "price": "620",
+            "name": "PoundsPerTonne",
+            "valuation": "£/T"
+        },
+        {
+            "idproducts": 1,
+            "price": "15.209375",
+            "name": "PoundsPerSheet",
+            "valuation": "£/Sht"
+        },
+        {
+            "idproducts": 1,
+            "price": "55",
+            "name": "PoundsPerTonne",
+            "valuation": "£/T"
+        },
+        {
+            "idproducts": 1,
+            "price": "33",
+            "name": "PoundsPerLength",
+            "valuation": "£/L"
+        },
+        {
+            "idproducts": 1,
+            "price": "33",
+            "name": "PoundsPerLength",
+            "valuation": "£/L"
+        },
+        {
+            "idproducts": 1,
+            "price": "1",
+            "name": "PoundsPerLength",
+            "valuation": "£/L"
+        },
+        {
+            "idproducts": 1,
+            "price": "22",
+            "name": "PoundsPerLength",
+            "valuation": "£/L"
+        },
+        {
+            "idproducts": 1,
+            "price": "32",
+            "name": "PoundsPerSheet",
+            "valuation": "£/Sht"
+        },
+        {
+            "idproducts": 1,
+            "price": "666",
+            "name": "PoundsPerLength",
+            "valuation": "£/L"
+        },
+        {
+            "idproducts": 1,
+            "price": "666",
+            "name": "PoundsPerLength",
+            "valuation": "£/L"
+        },
+        {
+            "idproducts": 1,
+            "price": "667",
+            "name": "PoundsPerMeter",
+            "valuation": "£/m"
+        }
+    ],
+    "areaQuantities": [
+        {
+            "id": 33,
+            "idproducts": 1,
+            "idcodesdimensions": 1,
+            "idarea": 1,
+            "iduser": 5,
+            "rowindex": null,
+            "quantity": "7",
+            "length": "2500",
+            "width": "1250",
+            "createdat": "2021-11-04T10:28:51.095916",
+            "areacode": "RH",
+            "areaname": "Rack House\r",
+            "initials": "tt"
+        },
+        {
+            "id": 34,
+            "idproducts": 1,
+            "idcodesdimensions": 1,
+            "idarea": 1,
+            "iduser": 5,
+            "rowindex": null,
+            "quantity": "7",
+            "length": "2500",
+            "width": "1250",
+            "createdat": "2021-11-04T15:49:20.022971",
+            "areacode": "RH",
+            "areaname": "Rack House\r",
+            "initials": "tt"
+        }
+    ],
+    "pricing": {
+        "tonne": null,
+        "sheet": null,
+        "meter": null,
+        "squareMeter": null,
+        "length": null,
+        "kgPerSqrtMeter": 7.5,
+        "kgPerSheet": 23437.5,
+        "poundsPerSheet": 14531.25,
+        "poundsPerMeter": null,
+        "poundsPerLength": null,
+        "kgPerMeter": null
+    }
+}
+
+
+
+
+
