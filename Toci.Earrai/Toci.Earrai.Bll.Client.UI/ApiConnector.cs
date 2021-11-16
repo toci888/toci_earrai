a276ea3410ed18d5d6c2f97686645b7765a45e8c
kacper8989 <kacper.wybierala1@gmail.com>
2021-11-16 16:29:06 +0100

    api connector extend

--- Toci.Earrai/Toci.Earrai.Bll.Client.UI/ApiConnector.cs
+++ Toci.Earrai/Toci.Earrai.Bll.Client.UI/ApiConnector.cs
@@ -25,6 +25,85 @@ namespace Toci.Earrai.Bll.Client.UI
             return ApiGet<ProductDto>("api/Product/GetProduct/" + productId);
         }
 
+        public virtual User GetUser(int userId)
+        {
+            return ApiGet<User>("api/");
+        }
+
+        public virtual Areaquantity PostAreaQuantities() //POST
+        {
+            return ApiGet<Areaquantity>("api/AreaQuantity/PostAreaQuantities");
+        }
+
+        public virtual Areaquantity UpdateAreaQuantity() //POST
+        {
+            return ApiGet<Areaquantity>("api/AreaQuantity/UpdateAreaQuantity");
+        }
+        public virtual Areaquantity DeleteAreaQuantity() //POST
+        {
+            return ApiGet<Areaquantity>("api/AreaQuantity/UpdateAreaQuantity");
+        }
+
+        public virtual Areaquantity DeleteUrl(int id) //POST
+        {
+            return ApiGet<Areaquantity>("api/AreaQuantity/" + id);
+        }
+
+        public virtual Product GetProductsEx() 
+        {
+            return ApiGet<Product>("api/Product/GetProductsEx");
+        }
+
+        public virtual List<Product> GetProductsFromWorksheet(int worksheetId, string phrase, string skip) // phrase, skip? nigdy nie uzywa tego
+        {
+            return ApiGet<List<Product>>("api/Product/GetProducts/" + worksheetId + "/" + phrase + "/" + skip);
+        }
+
+        public virtual List<Worksheet> GetWorksheets(int worksheetId, string phrase, string skip) // phrase, skip? all, from db
+        {
+            return ApiGet<List<Worksheet>>("api/Worksheet/GetAllWorksheetsFromDb/" + worksheetId + "/" + phrase + "/" + skip);
+        }
+
+        public virtual List<Area> GetAreas()
+        {
+            return ApiGet<List<Area>>("api/Areas");
+        }
+
+        public virtual List<Areasquantity> GetAreasQuantitiesByProduct(int productId)
+        {
+            return ApiGet<List<Areasquantity>>("api/AreasQuantities/GetAreasQuantitiesByProduct/" + productId);
+        }
+
+        public virtual List<Commision> GetCommisions(int productId, double price) //price?
+        {
+            return ApiGet<List<Commision>>("api/Commisions/GetCommisions?productId=" + productId + "&price=" + price);
+        }
+
+        public virtual Quoteandprice PostQuoteandPrice(int id) //POST
+        {
+            return ApiGet<Quoteandprice>("api/QuoteAndPrice/PostQuoteandPrice");
+        }
+
+        public virtual List<Vendor> GetVendors(int productId, double price) //price?
+        {
+            return ApiGet<List<Vendor>>("api/QuoteAndPrice/GetAllVendorsFromDb");
+        }
+
+        public virtual List<Quoteandprice> GetQuoteAndMetric()
+        {
+            return ApiGet<List<Quoteandprice>>("api/QuoteAndMetric");
+        }
+
+        public virtual List<Quoteandprice> GetQuotesAndPricesByProductId(int productId)
+        {
+            return ApiGet<List<Quoteandprice>>("api/QuoteAndPrice/QuoteAndPriceByProductId/" + productId);
+        }
+
+        public virtual List<Quoteandprice> GetAvailableValuesForSelectedOption() //TU TO NWM XD
+        {
+            return ApiGet<List<Quoteandprice>>("api/QuoteAndPrice/QuoteAndPriceByProductId/");
+        }
+
         protected virtual T ApiGet<T>(string url)
         {
             using (HttpClient hc = new HttpClient())
