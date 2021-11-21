878f6beb2a3a8f2c182fcb863bfe65758ff68598
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-11-21 15:54:18 +0100

    search new functionalities.

---
+++ Toci.Earrai/Toci.Earrai.Bll/Search/DescriptionProductSearch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Earrai.Bll.Models;
+using Toci.Earrai.Database.Persistence.Models;
+
+namespace Toci.Earrai.Bll.Search
+{
+    public class DescriptionProductSearch : SearchProductBase
+    {
+        public override List<ProductSearchResponseDto> Search(ProductSearchRequestDto request)
+        {
+            List<Product> result = ProductLogic.Select(p => p.Description == request.Value).ToList();
+
+            return ToProductSearchResponseDto(FilterBySearchQuery(request, result));
+        }
+    }
+}
