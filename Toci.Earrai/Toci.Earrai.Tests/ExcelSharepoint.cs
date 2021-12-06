41064f158faf42e816aa334eb6771162212b6440
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-12-06 19:28:44 +0100

    some improvements.

--- Toci.Earrai/Toci.Earrai.Tests/ExcelSharepoint.cs
+++ Toci.Earrai/Toci.Earrai.Tests/ExcelSharepoint.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Data.CData.ExcelServices;
 using System.Threading.Tasks;
 using System.Data;
 using Microsoft.Graph;
@@ -17,29 +16,7 @@ namespace Toci.Earrai.Tests
     [TestClass]
     public class ExcelSharepoint
     {
-        [TestMethod]
-        public void dupa()
-        {
-            ExcelServicesConnection excelServicesConnection = 
-                new ExcelServicesConnection(@"Url=https://gormleysengineering.sharepoint.com/:x:/r/3184Stock/_layouts/15/Doc.aspx?sourcedoc=%7B77D393F3-BD42-4236-9DC8-33A652DF302F%7D&file=3184%20Gor_Product_Category_List%20%20Images.xlsx&action=default&mobileredirect=true&cid=6a3ef899-21a3-4fd1-8005-eb861892a53b");
-
-            DataSet ds = new DataSet();
-
-            ExcelServicesDataAdapter edr = new ExcelServicesDataAdapter("select * from Alum", excelServicesConnection);
-
-            edr.Fill(ds);
-
-
-            //SharePointContext ents = new SharePointContext();
-            //var CalendarQuery = from Calendar in ents.Calendar
-            //                    orderby Calendar.Location
-            //                    select Calendar;
-
-
-
-            
-        }
-
+    
         [TestMethod]
         public void win()
         {
---
+++ Toci.Earrai/Toci.Earrai.Tests/Import/Excel/Excel.cs
@@ -0,0 +1,31 @@
+﻿//using ExcelDataReader;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Earrai.Tests.Import.Excel
+{
+    public class ExcelEx : ExcelClient
+    {
+        public virtual Dictionary<string, Dictionary<string, List<List<string>>>> RequestWorkbooksEx()
+        {
+            Dictionary<string, Dictionary<string, List<List<string>>>> result = new Dictionary<string, Dictionary<string, List<List<string>>>>();
+
+            //var rdr = ExcelReaderFactory.CreateOpenXmlReader(new FileStream("3184 GEng_Stock_Version Dated 2021-11-26.xlsx", FileMode.Open));
+            //Excel.ExcelClient ex = new ExcelClient();
+
+            //rdr.d
+
+            
+           // IronXL.WorkBook workBook = new IronXL.WorkBook(new FileStream(@"d:\3184 GEng_Stock_Version Dated 2021-11-26.xlsx", FileMode.Open));
+           //DataSet ds = workBook.ToDataSet();
+
+            return result;
+        }
+    }
+}
