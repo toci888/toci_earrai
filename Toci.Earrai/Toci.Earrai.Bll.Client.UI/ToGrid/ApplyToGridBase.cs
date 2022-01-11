952642b8b7ecf13d6b4dd7d844b482b6d11c570b
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2022-01-11 10:46:00 +0100

    apply to grid - new approach, some progress.

---
+++ Toci.Earrai/Toci.Earrai.Bll.Client.UI/ToGrid/ApplyToGridBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Earrai.Bll.Models;
+
+namespace Toci.Earrai.Bll.Client.UI.ToGrid
+{
+    public abstract class ApplyToGridBase
+    {
+        protected List<Func<ProductDto, FlattenedEntity>> WorksheetColumnsMap = new List<Func<ProductDto, FlattenedEntity>>();
+
+        public virtual List<FlattenedEntity> GetFlattenedProduct(ProductDto product)
+        {
+            List<FlattenedEntity> result = new List<FlattenedEntity>();
+
+            int i = 0;
+
+            foreach (Func<ProductDto, FlattenedEntity> delColumn in WorksheetColumnsMap)
+            {
+                FlattenedEntity fe = delColumn(product);
+                fe.GridPosition = i;
+
+                result.Add(fe);
+
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Toci.Earrai/Toci.Earrai.Bll.Client.UI/ToGrid/ApplyToGridManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Earrai.Bll.Models;
+
+namespace Toci.Earrai.Bll.Client.UI.ToGrid
+{
+    public class ApplyToGridManager
+    {
+        protected Dictionary<int, ApplyToGridBase> Map = new Dictionary<int, ApplyToGridBase>()
+        {
+            { WorksheetsIds.PLTandSHEET, new PltSheetApplyToGrid() },
+            { WorksheetsIds.Alum, new AlumApplyToGrid() },
+            { WorksheetsIds.MshandExpMetal, new MshExpMetalApplyToGrid() },
+            { WorksheetsIds.ChanandBms, new ChanBmsApplyToGrid() },
+            { WorksheetsIds.AnglesplusT, new AnglesTApplyToGrid() },
+            { WorksheetsIds.FLTS, new FLTSApplyToGrid() },
+            { WorksheetsIds.RHS, new RHSApplyToGrid() },
+            { WorksheetsIds.Tube_CHS, new TubeCHSApplyToGrid() },
+            { WorksheetsIds.Rnds_Sqrs_HolBar, new RndsSqrsHolBarApplyToGrid() },
+        };
+
+        public virtual ApplyToGridBase GetApplyToGridLogic(int worksheetId)
+        {
+            return Map.ContainsKey(worksheetId) ? Map[worksheetId] : null;
+        }
+    }
+}
