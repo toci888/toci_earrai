310378786c59ac7c18791ad751a86662356d4843
kacper8989 <kacper.wybierala1@gmail.com>
2022-02-05 18:14:16 +0100

    rename endpoint

--- Toci.Earrai/Toci.Earrai.Microservice/Controllers/PrivilegesController.cs
+++ Toci.Earrai/Toci.Earrai.Microservice/Controllers/PrivilegesController.cs
@@ -18,19 +18,19 @@ namespace Toci.Earrai.Microservice.Controllers
         {
         }
 
-        [HttpPost("increase")]
+        [HttpPost("Increase")]
         public User IncreasePrivileges(User user)
         {
             return Logic.IncreasePrivileges(user);
         }
 
-        [HttpPost("decrease")]
+        [HttpPost("Decrease")]
         public User DecreasePrivileges(User user)
         {
             return Logic.DecreasePrivileges(user);
         }
 
-        [HttpPost("change")]
+        [HttpPost("Change")]
         public User ChangePrivileges(User user, PrivilegesEnum privileges)
         {
             return Logic.ChangePrivileges(user, (int)privileges);
