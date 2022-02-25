c3d1a55dc4dc35bde2bd651b93b595c2c9aed6e3
kacper8989 <kacper.wybierala1@gmail.com>
2022-02-25 16:38:49 +0100

    Product names map

--- Toci.Errai.client-mobile2/components/Product_UtilTable.js
+++ Toci.Errai.client-mobile2/components/Product_UtilTable.js
@@ -4,20 +4,38 @@ import { productDetails as pd } from '../styles/productDetails'
 
 
 export default function Product_UtilTable(props) {
-    //console.log(props)
+
+    let dictionary = new Map([
+        ["poundsPerTonne", "£ per tonne"],
+        ["poundsPerSheet", "£ per sheet"],
+        ["poundsPerSquareMeter", "£ per square meter"],
+        ["poundsPerLength", "£ per length"],
+        ["kgPerMeter", "Kg per meter"],
+        ["totalMeters", "Total meters"],
+        ["stockTakeValue", "Stock Take Value"],
+        ["kgPerSqrtMeter", "Kg per Sqrt Meter"],
+        ["kgPerSheet", "Kg per sheet"],
+        ["totalWeight", "Total weight"],
+    ]);
+    //, , 
+    const x = (param) => {
+        return dictionary.get(param)? dictionary.get(param) : param;
+    }
+
     return (
         <View style={{marginBottom: 15}}>
             <View>
                 <Text style={{padding: 15, fontSize: 17, width: '100%', textAlign: 'center'}}> {props.name} </Text>
             </View>
-
             {
                 props.details && Object.keys(props.details).map( (value, key) => {
                     if(props.details[value] == null) return
+
+                    console.log(key, value); 
                     return (
                         <View key={key} style={pd.inlineContainer}>
                             <View style={[pd.inlineItem, pd.inlineItemLeft]}>
-                                <Text  style={{textAlign: 'right'}}>{value}</Text>
+                                <Text style={{textAlign: 'right'}}>{x(value)}</Text>
                             </View>
                             <View style={[pd.inlineItem, pd.inlineItemRight]}>
                                 <Text>{props.details[value]}</Text>
@@ -26,7 +44,6 @@ export default function Product_UtilTable(props) {
                     )
                 } )
             }
-
         </View>
     )
 }
