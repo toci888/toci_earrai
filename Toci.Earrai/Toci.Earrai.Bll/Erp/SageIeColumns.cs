986d069a197f0038825d77ab001e174b2c7280a9
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2022-02-28 18:15:36 +0100

    sage doc :P

--- Toci.Earrai/Toci.Earrai.Bll/Erp/SageIeColumns.cs
+++ Toci.Earrai/Toci.Earrai.Bll/Erp/SageIeColumns.cs
@@ -8,36 +8,36 @@ namespace Toci.Earrai.Bll.Erp
 {
     public static class SageIeColumns
     {
-        public const string ProductRecordAccountReference = "ProductRecord.AccountReference";
-        public const string ProductRecordDescription = "ProductRecord.Description";
-        public const string ProductRecordVATCode = "ProductRecord.VATCode";
-        public const string ProductRecordSalesPrice = "ProductRecord.SalesPrice";
-        public const string ProductRecordCostPriceStandard = "ProductRecord.CostPriceStandard";
-        public const string ProductRecordDateLastPurchase = "ProductRecord.DateLastPurchase";
-        public const string ProductRecordUnitOfSale = "ProductRecord.UnitOfSale";
-        public const string ProductRecordLocation = "ProductRecord.Location";
-        public const string ProductRecordSalesNominal = "ProductRecord.SalesNominal";
-        public const string ProductRecordCategoryNumber = "ProductRecord.CategoryNumber";
-        public const string ProductRecordBarCode = "ProductRecord.BarCode";
-        public const string ProductRecordDepartmentNumber = "ProductRecord.DepartmentNumber";
-        public const string ProductRecordSupplierAccountReference = "ProductRecord.SupplierAccountReference";
-        public const string ProductRecordPartNumber = "ProductRecord.PartNumber";
-        public const string ProductRecordQuantityReOrderLevel = "ProductRecord.QuantityReOrderLevel";
-        public const string ProductRecordCommodityCode = "ProductRecord.CommodityCode";
-        public const string ProductRecordWeight = "ProductRecord.Weight";
-        public const string ProductRecordStockTakeDate = "ProductRecord.StockTakeDate";
-        public const string ProductRecordQuantityReOrder = "ProductRecord.QuantityReOrder";
-        public const string ProductRecordQuantityLastStockTake = "ProductRecord.QuantityLastStockTake";
-        public const string ProductRecordInactiveFlag = "ProductRecord.InactiveFlag";
-        public const string ProductCategoryNumber = "ProductCategory.Number";
-        public const string ProductRecordCategoryName = "ProductRecord.CategoryName";
-        public const string ProductRecordWebCategory1 = "ProductRecord.WebCategory1";
-        public const string ProductRecordWebCategory2 = "ProductRecord.WebCategory2";
-        public const string ProductRecordWebCategory3 = "ProductRecord.WebCategory3";
-        public const string ProductRecordSupplementaryUnitQuantity = "ProductRecord.SupplementaryUnitQuantity";
+        public const string ProductRecordAccountReference = "ProductRecord.AccountReference"; // yes
+        public const string ProductRecordDescription = "ProductRecord.Description"; //yes
+        public const string ProductRecordVATCode = "ProductRecord.VATCode"; // ??
+        public const string ProductRecordSalesPrice = "ProductRecord.SalesPrice"; // Ł / tonne ?
+        public const string ProductRecordCostPriceStandard = "ProductRecord.CostPriceStandard"; // 0 ....
+        public const string ProductRecordDateLastPurchase = "ProductRecord.DateLastPurchase"; // empty in xlsx
+        public const string ProductRecordUnitOfSale = "ProductRecord.UnitOfSale"; // ea, ?
+        public const string ProductRecordLocation = "ProductRecord.Location"; // empty
+        public const string ProductRecordSalesNominal = "ProductRecord.SalesNominal"; // 4003 ?
+        public const string ProductRecordCategoryNumber = "ProductRecord.CategoryNumber"; // 1 , 3, ??
+        public const string ProductRecordBarCode = "ProductRecord.BarCode"; // empty
+        public const string ProductRecordDepartmentNumber = "ProductRecord.DepartmentNumber"; // 0
+        public const string ProductRecordSupplierAccountReference = "ProductRecord.SupplierAccountReference"; // empty
+        public const string ProductRecordPartNumber = "ProductRecord.PartNumber"; //empty
+        public const string ProductRecordQuantityReOrderLevel = "ProductRecord.QuantityReOrderLevel"; // 0
+        public const string ProductRecordCommodityCode = "ProductRecord.CommodityCode"; // 84313900, ?
+        public const string ProductRecordWeight = "ProductRecord.Weight"; // 0
+        public const string ProductRecordStockTakeDate = "ProductRecord.StockTakeDate"; // empty
+        public const string ProductRecordQuantityReOrder = "ProductRecord.QuantityReOrder"; // 0
+        public const string ProductRecordQuantityLastStockTake = "ProductRecord.QuantityLastStockTake"; // 0
+        public const string ProductRecordInactiveFlag = "ProductRecord.InactiveFlag"; // 0
+        public const string ProductCategoryNumber = "ProductCategory.Number"; // 1, 3 - ??
+        public const string ProductRecordCategoryName = "ProductRecord.CategoryName"; // yes, PL_Plate Mild Steel
+        public const string ProductRecordWebCategory1 = "ProductRecord.WebCategory1"; // 2
+        public const string ProductRecordWebCategory2 = "ProductRecord.WebCategory2"; //2500, length ?
+        public const string ProductRecordWebCategory3 = "ProductRecord.WebCategory3"; // 1250, width ? 
+        public const string ProductRecordSupplementaryUnitQuantity = "ProductRecord.SupplementaryUnitQuantity"; // 0
         public const string Column1 = "Column1";
-        public const string ProductRecordQuantityOnLastPurchaseOrder = "ProductRecord.QuantityOnLastPurchaseOrder";
-        public const string ProductRecordQuantityOnOrder = "ProductRecord.QuantityOnOrder";
+        public const string ProductRecordQuantityOnLastPurchaseOrder = "ProductRecord.QuantityOnLastPurchaseOrder"; // 0
+        public const string ProductRecordQuantityOnOrder = "ProductRecord.QuantityOnOrder"; // 0
 
     }
 }
