27f1f9b90c9be749938088195e1b96a8a98635ee
kacper8989 <kacper.wybierala1@gmail.com>
2022-07-11 14:37:54 +0200

    test endpoints

---
+++ Toci.Earrai/Toci.Earrai.Tests/Endpoints/AuthorizeTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Earrai.Tests.Endpoints
+{
+    [TestClass]
+    public class AuthorizeTest
+    {
+        [TestMethod]
+        public void UnitTest1()
+        {
+            HttpClient client = new HttpClient();
+
+            Task<HttpResponseMessage> response = client.GetAsync("http://127.0.0.1:8642/api/areas");
+            HttpResponseMessage x = response.Result;
+        }
+    }
+}
